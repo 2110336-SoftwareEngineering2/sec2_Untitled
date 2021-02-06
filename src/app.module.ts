0ad2675d6e0da9f68b162b4a6d438d1fc1ebe6cf
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-02-06 15:10:36 +0000

    initialize account, booking, review, search subfolder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetOwner } from './entities/petowner.entity';
 import { PetSitter } from './entities/petsitter.entity';
+import { AccountModule } from './account/account.module';
+import { ReviewModule } from './review/review.module';
+import { BookingModule } from './booking/booking.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
   imports: [
@@ -16,7 +20,7 @@ import { PetSitter } from './entities/petsitter.entity';
       database: 'se2',
       entities: [PetOwner, PetSitter],
       synchronize: true // this should be false in production
-    })
+    }),TypeOrmModule.forFeature([PetOwner]), AccountModule, ReviewModule, BookingModule, SearchModule
   ],
   controllers: [AppController],
   providers: [AppService]
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PetOwner } from './entities/petowner.entity';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectRepository(PetOwner) private readonly repo: Repository<PetOwner>){}
+
   getHello(): string {
     return 'Hello World!';
   }
+
 }
