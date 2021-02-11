ed94a519402779874fca5b4ca13d0c49b8b2925e
SuchonC <suchon_foke@windowslive.com>
2021-02-11 12:57:26 +0000

    Modify view names

--- src/booking/viewnames.ts
+++ src/booking/viewnames.ts
@@ -1,4 +1,4 @@
 export const viewNames = {
-    show_pet_sitter_info : "show_pet_sitter_info",
-    show_booking_options : "booking_options"
+    show_pet_sitter_info : "booking/pet_sitter_booking_info",
+    show_booking_options : "booking/booking_options"
 }
