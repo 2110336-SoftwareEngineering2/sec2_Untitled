dae6a979c6b15a0ffbbd477c08fd4a583a3045cf
SuchonC <suchon_foke@windowslive.com>
2021-02-13 16:25:11 +0000

    Add Pet and Booking Repo

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from 'src/entities/booking.entity';
+import { Pet } from 'src/entities/pet.entity';
 import { PetOwner } from 'src/entities/petowner.entity';
 import { PetSitter } from 'src/entities/petsitter.entity';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter])],
+  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking])],
   controllers: [BookingController],
   providers: [BookingService]
 })
