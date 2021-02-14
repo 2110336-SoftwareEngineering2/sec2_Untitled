93c5fd5429d15bede9307c127751072f555b6b9f
SuchonC <suchon_foke@windowslive.com>
2021-02-14 13:45:26 +0000

    Add repo sitterReview

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -4,11 +4,12 @@ import { Booking } from 'src/entities/booking.entity';
 import { Pet } from 'src/entities/pet.entity';
 import { PetOwner } from 'src/entities/petowner.entity';
 import { PetSitter } from 'src/entities/petsitter.entity';
+import { SitterReview } from 'src/entities/sitterreview.entity';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking])],
+  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking, SitterReview])],
   controllers: [BookingController],
   providers: [BookingService]
 })
