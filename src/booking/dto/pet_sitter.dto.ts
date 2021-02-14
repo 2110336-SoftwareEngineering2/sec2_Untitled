060bb4796fbc3f3cc3f48b1825f26c68aa36a888
SuchonC <suchon_foke@windowslive.com>
2021-02-14 13:46:08 +0000

    Improve Pet sitter DTO for booking info - Sitter reviews - Experiences - Services

--- src/booking/dto/pet_sitter.dto.ts
+++ src/booking/dto/pet_sitter.dto.ts
@@ -1,3 +1,18 @@
+import * as dayjs from "dayjs";
+import { PetSitter } from "src/entities/petsitter.entity";
+import { SitterReview } from "src/entities/sitterreview.entity";
+
+type sitterReview = {
+    owner: {
+        id: number,
+        fname: string,
+        lname: string,
+        picUrl: string,
+    },
+    description: string,
+    rating: number
+}
+
 export class BookPetSitterDto {
     id: number;
     fname: string;
@@ -9,7 +24,34 @@ export class BookPetSitterDto {
     location: string;
     picUrl: string;
     description: string;
+    reviews: sitterReview[];
+    services: string[]
 
-    public get fullName(): string { return this.fullName }
-    public get fullGender(): string { return this.fullGender }
+    exp: {
+        value: number,
+        unit: string
+    }
+    fullName: string;
+    fullGender: string;
+
+    constructor(ps: PetSitter, reviews: SitterReview[]){
+        // assign everything
+        Object.assign(this, ps)
+
+        // calculate year of experience
+        let now = dayjs()
+        let exp_y = now.diff(ps.signUpDate, "year")
+        let exp_m = now.diff(ps.signUpDate, "month")
+        if(exp_y > 1) this.exp = {value: exp_y, unit: "years"}
+        else if(exp_y == 1) this.exp = {value: exp_y, unit: "year"}
+        else if(exp_m > 1) this.exp = {value: exp_m, unit: "months"}
+        else if(exp_m == 1) this.exp = {value: exp_m, unit: "month"}
+        else this.exp = {value: -1, unit: "month"} // less than a month
+
+        this.reviews = reviews // assign reviews
+        this.services = ps.services.split(', ').slice(0, -1) // assign services
+
+        this.fullName = ps.fullName
+        this.fullGender = ps.fullGender
+    }
 }
