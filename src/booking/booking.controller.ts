5f1086868b7d44aff95cd50007fdeee783244e5a
SuchonC <suchon_foke@windowslive.com>
2021-02-17 15:59:14 +0000

    Update /book/:psid and /book/:psid/options logic

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post, Render, Req, Response } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Patch, Post, Render, Req, Response, UnauthorizedException } from '@nestjs/common';
 import { viewNames } from 'src/booking/viewnames';
 import { BookingService } from './booking.service';
 import { BookPetSitterDto } from './dto/pet_sitter.dto';
@@ -8,26 +8,58 @@ export class BookingController {
     constructor(
         private readonly bookingService: BookingService
     ){}
+    
+    // ------------ Test accept booking for pet sitter --------------------
+    @Get('my')
+    @Render('booking/test.hbs')
+    async dashboard(){
+        let requests = await this.bookingService.handleShowingRequestForPetSitter(2000001)
+        return {requests: requests}
+    }
+
+    @Patch()
+    sitterResponseBooking(@Req() req, @Body() body){
+        // body.booking_id
+        // req.uid -> this got sent with ajax because html is in the same domain as backend
+        return this.bookingService.handleBookingResponseForPetSitter(body.booking_id, body.action, req.uid)
+    }
+    // --------------------------------------------------------------------
 
     // show pet sitter info
     @Get(':pet_sitter_id')
     @Render(viewNames.show_pet_sitter_info)
-    async index(@Param('pet_sitter_id') psid: number) {
+    async index(@Param('pet_sitter_id') psid: number, @Req() req) {
+        if(!req.uid) throw new UnauthorizedException("Log in as a Pet owner first")
+        if(!this.bookingService.isValidPetOwnerId(req.uid)) throw new UnauthorizedException("This page is for pet owner")
         let ps_info = await this.bookingService.handlePetSitterInfo(psid)
-        return ps_info
+        let po = await this.bookingService.findPetOwnerById(req.uid)
+        return {pet_sitter: ps_info, pet_owner: po}
     }
 
     @Get(':pet_sitter_id/options')
     @Render(viewNames.show_booking_options)
-    async show_options(@Param('pet_sitter_id') psid: number){
-        let ownerId = 1000001; // this should be retrieved from auth
+    async show_options(@Param('pet_sitter_id') psid: number, @Req() req){
+        if(!req.uid) throw new UnauthorizedException("Log in as a Pet owner first")
+        if(!this.bookingService.isValidPetOwnerId(req.uid)) throw new UnauthorizedException("This page is for pet owner")
+        let ownerId = req.uid; // this should be retrieved from auth
+        let pet_owner = await this.bookingService.findPetOwnerById(ownerId)
         let pets = await this.bookingService.findPetsByOwnerId(ownerId)
-        return { pets: pets}
+        let pet_sitter = await this.bookingService.findPetSitterById(psid)
+        let exp = this.bookingService.calculatePetSitterExp(pet_sitter.signUpDate)
+        let services_list = pet_sitter.services.split(', ').slice(0, -1)
+        let out_sitter = Object(pet_sitter)
+        out_sitter.services = services_list
+        out_sitter.exp = exp
+        return { pet_owner: pet_owner, pets: pets, pet_sitter: out_sitter }
     }
 
+    // create requesting booking
+    // startDate endDate sitter pet[]
     @Post()
-    async book_pet_sitter(@Body() request: any) {
-        if(await this.bookingService.handleIncomingRequest(request)) return {
+    async book_pet_sitter(@Body() request: any, @Req() req) {
+        if(!req.uid) throw new UnauthorizedException("Please sign in as a Pet Owner and try again")
+        if(!this.bookingService.isValidPetOwnerId(req.uid)) throw new UnauthorizedException("Invalid Pet Owner ID")
+        if(await this.bookingService.handleIncomingRequest(request, req.uid)) return {
             code: HttpStatus.OK,
             status: true
         }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Booking } from 'src/entities/booking.entity';
 import { Pet } from 'src/entities/pet.entity';
 import { PetOwner } from 'src/entities/petowner.entity';
 import { PetSitter } from 'src/entities/petsitter.entity';
 import { Repository } from 'typeorm';
-import { PetDto } from './dto/pet.dto';
 import { BookPetSitterDto } from './dto/pet_sitter.dto';
 import * as dayjs from "dayjs";
 import { SitterReview } from 'src/entities/sitterreview.entity';
@@ -14,6 +13,7 @@ let customParseFormat = require('dayjs/plugin/customParseFormat')
 dayjs.extend(customParseFormat)
 
 const DATE_FORMAT = "DD/MM/YYYY"
+type BookingReponse = "ACCEPT" | "DENY"
 
 @Injectable()
 export class BookingService {
@@ -32,9 +32,15 @@ export class BookingService {
 
     async findPetSitterById(id: number): Promise<PetSitter>{
         let pet_sitter = await this.petSitterRepo.findOne(id)
-        if(!pet_sitter) throw new NotFoundException("Pet sitter not found, recheck id")
+        if(!pet_sitter) throw new NotFoundException("Pet sitter not found, recheck ID")
 
         return pet_sitter
+    }
+
+    async findPetOwnerById(id: number): Promise<PetOwner>{
+        let pet_owner = await this.petOwnerRepo.findOne(id)
+        if(!pet_owner) throw new NotFoundException("Pet Owner not found, recheck ID")
+        return pet_owner
     }
 
     async findPetsByOwnerId(id: number): Promise<Pet[]> {
@@ -49,8 +55,9 @@ export class BookingService {
     }
 
     async handlePetSitterInfo(psid: number): Promise<BookPetSitterDto>{
+        if(!this.isValidPetSitterId(psid)) throw new BadRequestException("Pet sitter ID is invalid")
         let ps = await this.findPetSitterById(psid)
-
+        if(!ps) throw new NotFoundException("Pet sitter ID doesn't match any record")
         // get sitter reviews
         let reviews = await this.sitterReviewRepo.find({
             relations: ['owner'],
@@ -63,9 +70,26 @@ export class BookingService {
         return result
     }
 
+    calculatePetSitterExp(signUpDate: Date){
+        let exp = undefined
+        let now = dayjs()
+        let exp_y = now.diff(signUpDate, "year")
+        let exp_m = now.diff(signUpDate, "month")
+        if(exp_y > 1) exp = {value: exp_y, unit: "years"}
+        else if(exp_y == 1) exp = {value: exp_y, unit: "year"}
+        else if(exp_m > 1) exp = {value: exp_m, unit: "months"}
+        else if(exp_m == 1) exp = {value: exp_m, unit: "month"}
+        else exp = {value: -1, unit: "month"} // less than a month
+        return exp
+    }
+
     // po requests -> ps confirms -> paid by po
     //  requesting      pending       completed  
-    async handleIncomingRequest(incoming_booking: any): Promise<any> {
+    async handleIncomingRequest(incoming_booking: any, poid: number): Promise<any> {
+        // check if id is a pet owner id
+        if(!poid) throw new UnauthorizedException("Pet owner id is required")
+        if(!this.isValidPetOwnerId(poid)) throw new UnauthorizedException("Pet owner ID is invalid")
+
         // store booking status requesting
         let price = (await this.findPetSitterById(incoming_booking.sitterId)).priceRate
         let startDate = dayjs(incoming_booking.startDate, DATE_FORMAT).format()
@@ -74,10 +98,47 @@ export class BookingService {
         incoming_booking.price = price
         incoming_booking.startDate = startDate
         incoming_booking.endDate = endDate
-        incoming_booking.owner = 1000001 // retrieve from auth
+        incoming_booking.owner = poid
 
-        if(await this.bookingRepo.save(incoming_booking)) return true
+        for(let i=0; i<incoming_booking.pets.length; i++){
+            let {pets, ...temp} = incoming_booking
+            temp.pet = incoming_booking.pets[i]
+            if(await this.bookingRepo.save(temp)) return true
+        }
 
         return false
+    }
+
+    async handleShowingRequestForPetSitter(psid: number){
+        if(!psid) throw new BadRequestException("Pet sitter ID is required")
+        if(!this.isValidPetSitterId(psid)) throw new BadRequestException("Pet sitter ID is invalid")
+        let requests = await this.bookingRepo.find({
+            relations: ['pet', 'owner'],
+            where: {
+                sitter: psid
+            }
+        })
+
+        return requests
+    }
+
+    handleBookingResponseForPetSitter(booking_id: number, action: BookingReponse, psid: number){
+        // verify psid
+        if(!psid) throw new BadRequestException("Pet sitter ID is required")
+        if(!this.isValidPetSitterId(psid)) throw new UnauthorizedException("Pet sitter ID is invalid")
+        // check if psid related to booking_id
+        // take action on booking_id
+    }
+
+    isValidPetSitterId(id: number): boolean{
+        let str_id = "" + id
+        if(str_id.length != 7 || str_id[0] != '2') return false
+        return true
+    }
+
+    isValidPetOwnerId(id: number): boolean{
+        let str_id = "" + id
+        if(str_id.length != 7 || str_id[0] != '1') return false
+        return true
     }
 }
