7b9464614cf82418bf29b16eca3072f7ba223ede
SuchonC <suchon_foke@windowslive.com>
2021-02-17 16:02:42 +0000

    JS Handler for /book/:psid/options - Send data required to make booking request

---
+++ public/javascripts/booking/bookingOptionsHandler.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#daterangepicker").daterangepicker({
+        locale: {
+            format: "DD/MM/YYYY"
+        },
+        singleDatePicker: false,
+        autoApply: true
+    })
+})
+
+$("#bookBtn").on('click', sendBookingRequest)
+
+function sendBookingRequest(){
+    let pets = getSelectedPets()
+    if(pets.length == 0) {alert("Select at least one pet"); return;}
+    let {startDate, endDate} = getStartAndEndDate()
+    if(startDate == '' || endDate == '') {} // do something
+
+    let psid = $("meta[name='pet-sitter-id'").attr('content')
+    $.ajax({
+        url: "/book",
+        method: "POST",
+        data: {
+            startDate: startDate,
+            endDate: endDate,
+            sitter: psid,
+            pets: pets
+        }
+    })
+}
+
+function getSelectedPets(){
+    let pets_tag = $("#selectedPetZone").children()
+    let pets = []
+    for(let i=0; i<pets_tag.length; i++){
+        pets.push(pets_tag.eq(i).attr("pet-id"))
+    }
+    return pets
+}
+
+function getStartAndEndDate(){
+    let [startDate, endDate] = $("#daterangepicker").val().split(' - ')
+    return {startDate, endDate}
+}
