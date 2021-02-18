9403e8bfa434d579b53b567aabd589a43ebe28dd
Vee46834 <veerawong46834@hotmail.co.th>
2021-02-18 05:40:39 +0000

    update reviewPage and Form

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,17 +1,29 @@
-import { Controller, Get, Response } from '@nestjs/common';
+import { Controller, Get, Post, Body, Response, Request, Param } from '@nestjs/common';
 import { ReviewService } from './review.service';
 
 @Controller()
 export class ReviewController {
     constructor(private readonly reviewService: ReviewService) { }
 
-    @Get('/review')
-    renderReview(@Response() res): any {
-        res.render('review/reviewTest')
+    @Get('/reviewForm/:petsitter_id')
+    async renderReview(@Response() res, @Param("petsitter_id") petsitter_id){
+		let object = await this.reviewService.handleReviewForm(petsitter_id);
+        res.render('review/reviewForm1', {petSitter: object.petSitter})
     }
 
-    @Get('/test')
-    renderUserReviews(@Response() res): any {
-        res.render('review/reviewAndRating')
+    @Get('/reviews/:petsitter_id')
+    async renderUserReviews(@Response() res, @Request() req, @Param("petsitter_id") petsitter_id) {
+        let object = await this.reviewService.handlePetsitterReview(petsitter_id, req.uid);
+        //console.log(object.reviews)
+        //console.log(object.petSitter)
+		let amount = await this.reviewService.updateReviewAmount(petsitter_id);
+        res.render('review/reviewAndRating', {reviews: object.reviews, petSitter: object.petSitter})
     }
+
+	@Post('/reviewForm')
+	async review_petsitter(@Body() dto){ 
+		console.log(dto)
+		return await this.reviewService.saveReview(dto.stars, dto.feedback,1000013,2000001);
+	}
+
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PetOwner } from 'src/entities/petowner.entity';
+import { PetSitter } from 'src/entities/petsitter.entity';
+import { SitterReview } from 'src/entities/sitterreview.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+    constructor(
+        @InjectRepository(PetOwner) private readonly petOwnerRepo: Repository<PetOwner>,
+        @InjectRepository(PetSitter) private readonly petSitterRepo: Repository<PetSitter>,
+        @InjectRepository(SitterReview) private readonly sitterReviewRepo: Repository<SitterReview>){}
+
+    async handlePetsitterReview(petsitter_id: number, petowner_id: number){
+        let reviews = await this.sitterReviewRepo.find({
+            relations:["owner"],
+            where: {sitter: petsitter_id}
+        })
+        let petSitter = await this.petSitterRepo.findOne({
+            where: {id: petsitter_id}
+        })
+        let petOwner = await this.petOwnerRepo.findOne({
+            where: {id: petowner_id}
+        })
+        return {reviews: reviews, petSitter: petSitter, petOwner: petOwner};
+    }
+
+	 async handleReviewForm(petsitter_id: number){
+		 let petSitter = await this.petSitterRepo.findOne({
+            where: {id: petsitter_id}
+        })
+		 console.log(petSitter)
+		 return {petSitter: petSitter};
+	 }
+	
+	async saveReview(reviewRating:number, reviewDescription:string, petowner_id:number, petsitter_id:number){
+		let petSitter = await this.petSitterRepo.findOne({
+            where: {id: petsitter_id}
+        })
+        let petOwner = await this.petOwnerRepo.findOne({
+            where: {id: petowner_id}
+        })
+		//import randomInt tool
+		const randomInt = require('random-int');
+		const uid = randomInt(100,999)
+		
+		const review = {id:uid, rating:reviewRating, description: reviewDescription, owner: petOwner ,sitter: petSitter }
+		console.log(review)
+		return await this.sitterReviewRepo.save(review);
+	}
+	
+    async updateReviewAmount(petsitter_id: number){
+         let count = (await this.sitterReviewRepo.find({
+            where: {sitter: petsitter_id}
+         })).length
+         console.log(count)
+     }
+	
+}
