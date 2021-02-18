05c3362978baaa63ca1882ed5ce5709448bc4806
suchada hnoonpakdee <suchada.hnpd@gmail.com>
2021-02-18 05:32:06 +0000

    adding html elements /1

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller , Post , Get , Body , Delete , Param , Response } from '@nestjs/common';
+
+import {SearchService} from './search.service';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { PetOwner } from 'src/entities/petowner.entity';
+import { PetSitter } from 'src/entities/petsitter.entity';
+import { Booking } from 'src/entities/booking.entity';
+
 
 @Controller('search')
-export class SearchController {}
+export class SearchController {
+	
+	constructor (private searchservice : SearchService){ //เดี๋ยวจะมีคนส่ง object เข้ามาให้ใช้งาน
+		
+		
+		
+	}
+	
+	@Get()
+	getSearch(){
+		return this.searchservice.getSeachValue()
+	}
+	@Post()
+	searchTodo(@Body("location") location : string , @Body("Pet_type") Pet_type : string , @Body("start_date") start_date :Date , @Body("end_date") end_date : Date){
+		this.searchservice.searchTodo(location,Pet_type,start_date ,end_date)
+		
+		return this.searchservice.getSeachValue()
+	}
+
+	//for update delete
+	@Delete(":/id")
+	deleteTodoById(@Param("id") id : string){
+		console.log(`id : ${id}`)
+		return `id is ${id}`
+	}
+}
---
+++ src/search/searchvalue.entity.ts
@@ -0,0 +1,9 @@
+export class searchvalue{
+		
+		id:string;
+		location : string;
+		Pet_type : string;
+		start_date : Date;
+	 	end_date : Date;
+	
+}
