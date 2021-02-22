a6543a00e3f9f53f3db323d4b6d8c0bfd326ebcd
Vee46834 <veerawong46834@hotmail.co.th>
2021-02-22 18:54:36 +0000

    update reviewForm for petsitter

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PetOwner } from 'src/entities/petowner.entity';
 import { PetSitter } from 'src/entities/petsitter.entity';
 import { SitterReview } from 'src/entities/sitterreview.entity';
+import { OwnerReview } from 'src/entities/ownerreview.entity';
 import { ReviewController } from './review.controller';
 import { Booking } from 'src/entities/booking.entity'
 import { ReviewService } from './review.service';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([PetOwner, PetSitter, SitterReview, Booking])],
+  imports : [TypeOrmModule.forFeature([PetOwner, PetSitter, SitterReview, OwnerReview, Booking])],
   controllers: [ReviewController],
   providers: [ReviewService]
 })
