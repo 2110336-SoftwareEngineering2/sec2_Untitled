7910ef7f513bf62bb4cf997fca4dff229fb8be12
SuchonC <suchon_foke@windowslive.com>
2021-03-13 21:27:23 +0700

    Import notification service

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -5,11 +5,13 @@ import { Pet } from 'src/entities/pet.entity';
 import { PetOwner } from 'src/entities/petowner.entity';
 import { PetSitter } from 'src/entities/petsitter.entity';
 import { SitterReview } from 'src/entities/sitterreview.entity';
+import { Transaction } from 'src/entities/transaction.entity';
+import { NotificationModule } from 'src/notification/notification.module';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking, SitterReview])],
+  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking, SitterReview, Transaction]), NotificationModule],
   controllers: [BookingController],
   providers: [BookingService]
 })
