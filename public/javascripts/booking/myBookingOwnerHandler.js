724b78f207e780adf416a1c9b71858b18049e856
SuchonC <suchon_foke@windowslive.com>
2021-03-13 18:24:34 +0700

    Fade out after success fully cancel a booking

--- public/javascripts/booking/myBookingOwnerHandler.js
+++ public/javascripts/booking/myBookingOwnerHandler.js
@@ -1,9 +1,13 @@
 $("button[name='cancelBooking'").on('click', function(){
+    let parent = $(this).parents("div").closest("[name='booking_parent'")
     let booking_id = $(this).attr('booking-id')
     $.ajax({
         url: `/book/my/petowner/${booking_id}`,
         method: "PATCH"
     }).done(function(data){
-        console.log(data)
+        if(data.success) parent.fadeOut()
+        else {
+            alert(data.message)
+        }
     })
 })
