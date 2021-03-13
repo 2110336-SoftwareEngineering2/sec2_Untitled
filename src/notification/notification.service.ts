98511f5674901824f8c13f3bb275548406b63c05
SuchonC <suchon_foke@windowslive.com>
2021-03-13 21:25:00 +0700

    Notification module simple functions

---
+++ src/notification/notification.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from 'src/entities/transaction.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class NotificationService {
+    constructor(
+        @InjectRepository(Transaction)
+        private readonly transactionRepo: Repository<Transaction>,
+    ){}
+
+    // get all transaction for receiverId
+    async getNotificationsFor(receiverId: number){
+        let results = await this.transactionRepo.find({
+            where: {receiverId: receiverId}
+        })
+
+        return results
+    }
+
+    // get all tracsantion for receiverId since "date"
+    getNotificationsSince(){
+
+    }
+}
