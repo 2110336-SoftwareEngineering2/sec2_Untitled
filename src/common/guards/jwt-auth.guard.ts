f5c2bc33d48d6246028445cf4023c672decbe62e
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-16 15:42:09 +0000

    Move guards to dedicated folder

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -11,9 +11,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
   handleRequest(err, user, info) {
     // You can throw an exception based on either "info" or "err" arguments
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
+    if (err || !user) throw err || new UnauthorizedException();
     return user;
   }
 }
