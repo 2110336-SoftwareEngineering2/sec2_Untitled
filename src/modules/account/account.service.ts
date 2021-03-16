61e6f5f15253d8df2c2eb124962b2a4c7c959534
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-16 15:25:39 +0000

    Remove unnecessary console.log and add meaningful comment

--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -11,39 +11,22 @@ export class AccountService {
     constructor(
         @InjectRepository(PetOwner) private readonly petOwnerRepo: Repository<PetOwner>,
         @InjectRepository(PetSitter) private readonly petSitterRepo: Repository<PetSitter>,
-        @InjectRepository(Pet) private readonly petRepo: Repository<Pet>){}
-
-
-    // Profile
-    renderAccount(@Res() res){
-        return res.redner('')
-    }
-
+        @InjectRepository(Pet) private readonly petRepo: Repository<Pet>
+    ){}
 
     // CREATE
-
     async saveToRepo(role: string, user: PetOwner | PetSitter) : Promise<PetOwner | PetSitter> {
-        if (role === 'owner') {
-            console.log('saving owner')
-            return await this.petOwnerRepo.save(user);
-        }
-        else if (role === 'sitter') {
-            console.log('saving')
-            return await this.petSitterRepo.save(user);
-        }
+        if (role === 'owner') return await this.petOwnerRepo.save(user);
+        else if (role === 'sitter') return await this.petSitterRepo.save(user);
+        // ! this code should never be reached
         return null
     }
 
     async createAccount(role: string, dto: Omit<PetOwner | PetSitter, 'id'>) : Promise<PetOwner | PetSitter> {
-        console.log('in createAccount')
-        console.log('role ', role)
         const user = await this.findAccountByUsername(role,dto.username)
-        console.log('after find account')
         if (user) throw new BadRequestException('This username is already exist');
         else {
-            console.log('user name is not used')
-            console.log('dto',dto)
-            let newUser = null
+            let newUser;
             const password = await hash(dto.password, 10);
             if (role === 'owner') newUser  = { ... new PetOwner(), ... dto, password};
             else if (role === 'sitter') newUser = { ... new PetSitter(), ... dto, password}
@@ -57,18 +40,19 @@ export class AccountService {
     async findAccountById(role: string, id: number): Promise<any> {
         if (role === 'sitter') return await this.petSitterRepo.findOne(id);
         else if (role === 'owner') return await this.petOwnerRepo.findOne(id);
+        // ! this code should never be reached
         return null;
     }
 
     async findAccountByUsername(role: string, username: string): Promise<any> {
         if (role === 'sitter') return await this.petSitterRepo.findOne({username});
         else if (role === 'owner') return await this.petOwnerRepo.findOne({username});
+        // ! this code should never be reached
         return null;
     }
 
     async findPetbyId(role: string, id: number): Promise<any> {
-        let owner = id;
-        if (role === 'owner')return await this.petRepo.find({owner});
+        if (role === 'owner')return await this.petRepo.find({id});
         return null;
     }
 
@@ -80,9 +64,8 @@ export class AccountService {
         return await this.saveToRepo(role, user);
     }
 
-    async createPet(dto: Omit<Pet, 'id'|'owner'>, @Req() req) : Promise<Pet> {
-        let owner = req.user.id;
-        let newPet = {... new Pet(), ... dto, owner};
+    async createPet(dto: Omit<Pet, 'id'|'owner'>, @Req() {user: {id}}) : Promise<Pet> {
+        let newPet = {... new Pet(), ... dto, id};
         return this.petRepo.save(newPet)
     }
 }
