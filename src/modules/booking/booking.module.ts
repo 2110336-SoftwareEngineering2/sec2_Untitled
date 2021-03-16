238b144e133dcc3751bd50285231447d146d8a2b
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-16 16:01:17 +0000

    One line import

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Booking } from 'src/entities/booking.entity';
-import { Pet } from 'src/entities/pet.entity';
-import { PetOwner } from 'src/entities/petowner.entity';
-import { PetSitter } from 'src/entities/petsitter.entity';
-import { SitterReview } from 'src/entities/sitterreview.entity';
-import { Transaction } from 'src/entities/transaction.entity';
+import {Booking, Pet, PetOwner, PetSitter,SitterReview, Transaction} from 'src/entities'
 import { NotificationModule } from 'src/modules/notification/notification.module';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
