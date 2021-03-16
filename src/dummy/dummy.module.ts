79e54419a81c2b2c885c3cdf393820471198de79
SuchonC <suchon_foke@windowslive.com>
2021-03-16 10:26:38 +0000

    reset

--- src/dummy/dummy.module.ts
+++ src/dummy/dummy.module.ts
@@ -4,11 +4,12 @@ import { OwnerReview } from 'src/entities/ownerreview.entity';
 import { Pet } from 'src/entities/pet.entity';
 import { PetOwner } from 'src/entities/petowner.entity';
 import { PetSitter } from 'src/entities/petsitter.entity';
+import { SitterAnimal } from 'src/entities/sitteranimal.entity';
 import { SitterReview } from 'src/entities/sitterreview.entity';
 import { DummyController } from './dummy.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PetSitter, PetOwner, Pet, SitterReview, OwnerReview])],
+  imports: [TypeOrmModule.forFeature([PetSitter, PetOwner, Pet, SitterReview, OwnerReview, SitterAnimal])],
   controllers: [DummyController]
 })
 export class DummyModule {}
