abe6dccc824622492ac3880230c0a283ad677aba
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-16 16:34:38 +0000

    One line imports

--- src/modules/review/review.module.ts
+++ src/modules/review/review.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PetOwner } from 'src/entities/petowner.entity';
-import { PetSitter } from 'src/entities/petsitter.entity';
-import { SitterReview } from 'src/entities/sitterreview.entity';
-import { OwnerReview } from 'src/entities/ownerreview.entity';
+import {PetOwner, PetSitter, SitterReview, OwnerReview, Booking} from 'src/entities'
 import { ReviewController } from './review.controller';
-import { Booking } from 'src/entities/booking.entity'
 import { ReviewService } from './review.service';
 
 @Module({
