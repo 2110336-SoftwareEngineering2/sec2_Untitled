45ba3a09e94e73c68ac8dac0f29e3484aae2d3d9
Amnard <authorites@outlook.co.th>
2021-03-17 11:29:24 +0000

    edit noti

--- public/javascripts/registerPet.js
+++ public/javascripts/registerPet.js
@@ -15,3 +15,30 @@ $(".function-icon").hover(function(){
             $(this).find(".activity-icon").css("color", "white");
       }
   });
+
+  $('#cancel_button').click(function(){
+      window.location.href = "https://se2-ypebr.run.goorm.io/account";
+  })
+
+  $('#save_button').click(function(){
+        let newPet = {
+              name: $('#name').val(),
+              type: $(`.selected-icon`).attr('value'),
+              gender: $('input[name="inlineRadioOptions"]:checked').val(),
+              birthdate: $('#daterangepicker').val(),
+              apperance: $('#appearance').val()
+        }
+        alert();    
+  })
+
+  $(document).ready(function(){
+      $("#daterangepicker").daterangepicker({
+          locale: {
+              format: "DD/MM/YYYY"
+          },
+          startDate: dayjs().format("DD/MM/YYYY"),
+          singleDatePicker: true,
+          showDropdowns: true,
+          autoApply: true
+      })
+  })
