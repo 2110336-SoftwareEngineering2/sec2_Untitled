72b7359e86bf7929d039254affab5af60888f4e1
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-23 17:48:01 +0000

    Add employee repo dependency

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -4,12 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { jwtConstants } from 'src/modules/authentication/constants';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
-import {Pet, PetOwner, PetSitter} from 'src/entities'
+import {Employee, Pet, PetOwner, PetSitter} from 'src/entities'
 import { NotificationService } from '../notification/notification.service';
 import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([PetOwner]),TypeOrmModule.forFeature([PetSitter]),TypeOrmModule.forFeature([Pet]), JwtModule.register({
+  imports : [TypeOrmModule.forFeature([PetOwner]),TypeOrmModule.forFeature([PetSitter]),TypeOrmModule.forFeature([Pet]), TypeOrmModule.forFeature([Employee]), JwtModule.register({
     secret: jwtConstants.secret,
     signOptions: {expiresIn: '1d'}
   }), NotificationModule ],
