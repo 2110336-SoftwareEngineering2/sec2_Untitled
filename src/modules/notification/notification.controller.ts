319a21c281ad6847534bcf4f6f85541acc0258ed
SuchonC <suchon_foke@windowslive.com>
2021-03-24 14:31:41 +0700

    Add route for getting notifications

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { NotificationService } from './notification.service';
 
-@Controller('notification')
-export class NotificationController {}
+@Controller()
+export class NotificationController {
+    constructor(
+        private readonly notificationService: NotificationService
+    ) { }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Get('/notifications')
+    @Roles('sitter', 'owner')
+    getNotifications(@Req() { user: { id } }) {
+        return this.notificationService.getNotificationsFor(id)
+    }
+}
