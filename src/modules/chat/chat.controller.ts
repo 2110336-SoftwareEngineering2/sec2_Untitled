a892105a38c0445439dff05b27a57971ca74a8a5
SuchonC <suchon_foke@windowslive.com>
2021-03-25 21:07:39 +0700

    Able to save message

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { ChatService } from './chat.service';
 
-@Controller('chat')
-export class ChatController {}
+@Controller()
+@UseGuards(JwtAuthGuard)
+export class ChatController {
+    constructor(
+        private readonly chatService: ChatService
+    ) { }
+
+    @Post('/chat')
+    @UseGuards(RolesGuard)
+    @Roles('sitter', 'owner')
+    // {
+    //     receiverId: number,
+    //     message: string
+    // }
+    incomingMessage(@Req() { user: { id } }, @Body() { receiverId, message }) {
+        // id is the id of sender
+        return this.chatService.handleIncomingMessage(id, receiverId, message)
+    }
+}
