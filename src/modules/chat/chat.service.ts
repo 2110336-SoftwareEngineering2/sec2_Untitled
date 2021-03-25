d2d76ccc58b2c171158e5393e19a42dc9a4b47fe
SuchonC <suchon_foke@windowslive.com>
2021-03-25 22:22:42 +0700

    Chat now able to retrieve message since arbitrary time

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PetOwner, PetSitter } from 'src/entities';
 import { Message } from 'src/entities/message.entity';
-import { Repository } from 'typeorm';
+import { MoreThan, Repository } from 'typeorm';
+import * as dayjs from "dayjs";
+import * as customParseFormat from 'dayjs/plugin/customParseFormat'
+import * as utc from 'dayjs/plugin/utc'
+import { AccountService } from '../account/account.service';
+dayjs.extend(utc)
+dayjs.extend(customParseFormat)
 
 @Injectable()
 export class ChatService {
@@ -12,7 +18,8 @@ export class ChatService {
         @InjectRepository(PetOwner)
         private readonly petOwnerRepo: Repository<PetOwner>,
         @InjectRepository(PetSitter)
-        private readonly petSitterRepo: Repository<PetSitter>
+        private readonly petSitterRepo: Repository<PetSitter>,
+        private readonly accountService: AccountService
     ) { }
 
     // save message into DB
@@ -20,16 +27,40 @@ export class ChatService {
         // validate receiverId (no need for senderId because it came from JwtAuthGuard not user's input)
         let result = await this.messageRepo.save({ senderId, receiverId, message })
         if (result) return { success: true }
-        return { success: false, message: "Error occured when saving your message"}
+        return { success: false, message: "Error occured when saving your message" }
     }
 
     // retrieve messages from DB corresponding to input receiver ID
-    getMessagesFor(receiverId){
-        
+    getMessagesFor(receiverId) {
+
     }
 
     // retrieve messages from DB corresponding to input receiver ID since input time
-    getMessageSince(){
-        
+    async getMessageSince(receiverId, since) {
+        // validate "since" format to be in "DD/MM/YYYY HH:mm:ss" utc
+        if (!dayjs(since, "DD/MM/YYYY HH:mm:ss", true).isValid()) return {
+            success: false,
+            message: "Expected since format is \"DD/MM/YYYY HH:mm:ss\" in UTC time"
+        }
+        let sinceUtcFormat = dayjs(since, "DD/MM/YYYY HH:mm:ss").utc().format()
+        let messages = Object(await this.messageRepo.find({
+            createDatetime: MoreThan(sinceUtcFormat),
+            receiverId: receiverId
+        }))
+
+        // retrieve sender info for each message
+        for (let i = 0; i < messages.length; i++) {
+            messages[i].sender = await this.getSenderInfo(messages[i].senderId)
+        }
+
+        return messages
+    }
+
+    async getSenderInfo(id) {
+        let strId = String(id)
+        // pet owner
+        if (strId[0] == '1') return await this.accountService.findAccountById("owner", id)
+        // pet sitter
+        else if (strId[0] == '2') return await this.accountService.findAccountById("sitter", id)
     }
 }
