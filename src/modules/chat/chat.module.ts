4d48aec7216eaffc5f282d21d0b726e02e4de81c
SuchonC <suchon_foke@windowslive.com>
2021-03-25 21:07:57 +0700

    Import TypeORM in ChatModule

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PetOwner, PetSitter } from 'src/entities';
+import { Message } from 'src/entities/message.entity';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Message, PetOwner, PetSitter])],
   controllers: [ChatController],
   providers: [ChatService]
 })
