343aab00036054a6df74795d5f950775a4e7cc7b
patipanb <patipan.b7@gmail.com>
2021-03-26 23:57:17 +0700

    initialize supportModule

--- src/modules/support/support.module.ts
+++ src/modules/support/support.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PetOwner, PetSitter, SitterReview, OwnerReview, Booking, Report } from 'src/entities';
+import { NotificationModule } from '..';
 import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([PetOwner, PetSitter, SitterReview, OwnerReview, Booking, Report]), NotificationModule],
   controllers: [SupportController],
-  providers: [SupportService]
+  providers: [SupportService],
+  exports: [SupportService]
 })
 export class SupportModule {}
