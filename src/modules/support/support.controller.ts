310c4bfc81ef45a95c159941a487f1252da066ea
patipanb <patipan.b7@gmail.com>
2021-03-26 23:57:00 +0700

    initialize supportController

--- src/modules/support/support.controller.ts
+++ src/modules/support/support.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Response, Request, Param, UseGuards, Req } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { SupportService } from './support.service';
 
-@Controller('support')
-export class SupportController {}
+
+@Controller("support")
+export class SupportController {
+    constructor(private readonly supportService: SupportService) { }
+
+    
+    @Get()
+    async renderSupportPage(@Response() res, @Request() req) {
+        await this.supportService.showAllReport();
+        res.render('support/showReports')
+    }
+}
+    
