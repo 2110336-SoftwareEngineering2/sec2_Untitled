33c5f2b81751ecafe7878b83d5d5c570f7b44cd8
patipanb <patipan.b7@gmail.com>
2021-03-26 23:56:41 +0700

    initial supportService

--- src/modules/support/support.service.ts
+++ src/modules/support/support.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PetOwner, PetSitter, Booking, SitterReview, OwnerReview, Report } from 'src/entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SupportService {}
+export class SupportService {
+    constructor(
+        
+		){}
+
+        async showAllReport(){
+            console.log("This is report page")
+        }
+}
