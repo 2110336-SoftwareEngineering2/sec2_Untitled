9edf33ae7ccfc8c5058d86ea3407ae0b6cc0944e
patipanb <patipan.b7@gmail.com>
2021-03-30 00:39:50 +0700

    implement function renderSupportPage

--- src/modules/support/support.controller.ts
+++ src/modules/support/support.controller.ts
@@ -3,16 +3,18 @@ import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { SupportService } from './support.service';
 
-
+//@UseGuards(JwtAuthGuard)
 @Controller("support")
 export class SupportController {
     constructor(private readonly supportService: SupportService) { }
 
-    
+    //@UseGuards(JwtAuthGuard, RolesGuard)
+    //@Roles('admin')
     @Get()
     async renderSupportPage(@Response() res, @Request() req) {
-        await this.supportService.showAllReport();
-        res.render('support/showReports')
+        let reportLists = await this.supportService.getAllReports();
+        //console.log("this is report lists",reportLists);
+        res.render('support/showReports', {reports: reportLists})
     }
 }
     
