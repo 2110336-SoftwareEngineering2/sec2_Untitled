c2e44c98ed3a5d33d0bee37832278f6bad2cc4fd
Pabhabuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-31 18:10:01 +0000

    Remove unused imports

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking, Pet, PetOwner, PetSitter, SitterReview, Transaction } from 'src/entities'
 import { NotificationModule } from 'src/modules/notification/notification.module';
+import { AccountModule } from '../account/account.module';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking, SitterReview, Transaction]), NotificationModule],
+  imports: [TypeOrmModule.forFeature([PetOwner, PetSitter, Pet, Booking, SitterReview, Transaction]), NotificationModule, AccountModule],
   controllers: [BookingController],
   providers: [BookingService]
 })
