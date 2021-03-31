edfb826a3de0cc4c6fe25363aa162b4043409341
SuchonC <suchon_foke@windowslive.com>
2021-03-31 23:08:04 +0700

    Add a second to latestUpdate (Not a good solution)

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -20,6 +20,7 @@ export class ChatService {
     // save message into DB
     async handleSaveMessage(senderId, receiverId, message) {
         // validate receiverId (no need for senderId because it came from JwtAuthGuard not user's input)
+        if (message == '') return { success: false, message: "Message cannot be empty" }
         let result = await this.messageRepo.save({ senderId, receiverId, message })
         if (result) return { success: true }
         return { success: false, message: "Error occured when saving your message" }
@@ -34,15 +35,15 @@ export class ChatService {
             ]
         }))
 
+        let latestUpdate = dayjs.utc().format("DD/MM/YYYY HH:mm:ss") // default is current time
         for (let i = 0; i < messages.length; i++) {
             if (messages[i].senderId == requestingUser) messages[i].isMe = true
             else {
                 messages[i].isMe = false
                 messages[i].sender = await this.getSenderInfo(messages[i].senderId)
             }
+            if (i == messages.length - 1) latestUpdate = dayjs(messages[i].createDatetime).add(1, 'second').format("DD/MM/YYYY HH:mm:ss")
         }
-
-        let latestUpdate = dayjs.utc().format("DD/MM/YYYY HH:mm:ss")
 
         return { success: true, latestUpdate, messages }
     }
@@ -55,14 +56,16 @@ export class ChatService {
             message: "Expected since format is \"DD/MM/YYYY HH:mm:ss\" in UTC time"
         }
 
-        let sinceUtcFormat = dayjs(since, "DD/MM/YYYY HH:mm:ss").utc().format()
+        // set utcOffSet to 0 because the incoming "since" is already in utc
+        let sinceInUtcFormat = dayjs(since, "DD/MM/YYYY HH:mm:ss").utcOffset(0, true).format()
         let messages = Object(await this.messageRepo.find({
             where: [
-                { senderId: requestingUser, receiverId: otherUser, createDatetime: MoreThan(sinceUtcFormat) },
-                { senderId: otherUser, receiverId: requestingUser, createDatetime: MoreThan(sinceUtcFormat) }
+                { senderId: requestingUser, receiverId: otherUser, createDatetime: MoreThan(sinceInUtcFormat) },
+                { senderId: otherUser, receiverId: requestingUser, createDatetime: MoreThan(sinceInUtcFormat) }
             ]
         }))
 
+        let latestUpdate = dayjs.utc().format("DD/MM/YYYY HH:mm:ss") // default is current time
         // retrieve sender info for each message
         for (let i = 0; i < messages.length; i++) {
             if (messages[i].senderId == requestingUser) messages[i].isMe = true
@@ -70,9 +73,11 @@ export class ChatService {
                 messages[i].isMe = false
                 messages[i].sender = await this.getSenderInfo(messages[i].senderId)
             }
+            // add one second to prevent querying the messages that are already been sent to the client
+            // Problem is because createDatetime stored in datacase has 6 digits millisecond
+            // with dayjs you can only use 3 ex. 12:00.123456 > 12:00.123
+            if (i == messages.length - 1) latestUpdate = dayjs(messages[i].createDatetime).add(1, 'second').format("DD/MM/YYYY HH:mm:ss")
         }
-
-        let latestUpdate = dayjs.utc().format("DD/MM/YYYY HH:mm:ss")
 
         return { success: true, latestUpdate, messages }
     }
