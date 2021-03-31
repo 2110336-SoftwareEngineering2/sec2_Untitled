38230bd0db78e26be43ffa6e2733d79122023344
Pabhabuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-31 18:10:12 +0000

    Remove unused file

--- src/modules/notificationGuard/notificationGuard.module.ts
+++
@@ -1,9 +0,0 @@
-import {Module,Global} from '@nestjs/common';
-import {NotificationService} from '../notification/notification.service';
-
-@Global()
-@Module({
-    providers: [ NotificationService ],
-    exports: [NotificationService]
-})
-export class NotificationGuardModule {}
