f2afe5e0591d86df161fc944157f20d2068afc54
Pabhabuwat Pongsawad <pabhanuwat@gmail.com>
2021-03-31 15:14:00 +0000

    Refactor codes

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,16 +1,10 @@
 var source = document.getElementById("entry-template").innerHTML;
 var template = Handlebars.compile(source);
-var context = { notifications: [{ description: "desc", performerPicUrl: "someurl", createDatetime: new Date() }] };
-var html = template(context);
-document.getElementById('output').innerHTML = html
 
 $.ajax({
     url: '/notifications',
     method: 'GET'
 }).done(data => {
-    console.log("DATA received")
-    const notifications = data
-    const html = template({notifications});
-    console.log(html)
+    const html = template({notifications: data});
     $('#output').html(html)
 })
