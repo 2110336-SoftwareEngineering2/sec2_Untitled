488abd26ab3383667f6c5d5c4e05fe381ebba1da
patipanb <patipan.b7@gmail.com>
2021-04-07 00:24:30 +0700

    add POST method resolveReport in controller

--- src/modules/support/support.controller.ts
+++ src/modules/support/support.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, Post, Body, Response, Request, Param, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Response, Request, Param, UseGuards, Req, Redirect } from '@nestjs/common';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { SupportService } from './support.service';
 
 @UseGuards(JwtAuthGuard,RolesGuard)
-@Roles('admin')
 @Controller("support")
 export class SupportController {
     constructor(private readonly supportService: SupportService) { }
 
     @Get()
+    @Roles('admin')
     async renderSupportPage(@Response() res) {
         let reportLists = await this.supportService.getAllReports();
         res.render('support/showReports', {reports: reportLists})
+    }
+
+    @Roles('admin')
+    @Post('/resolveReport/:report_id')
+    async resolveReport(@Response() res, @Param("report_id") report_id){
+        console.log(report_id)
+        await this.supportService.updateReportStatus(report_id)
+        console.log("Successfully Clear Report id", report_id)
+        res.redirect("/support")
     }
 }
     
